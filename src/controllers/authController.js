2038891af3056e7859eb8867904caa408aba4d35
antonio <antoniorc7@gmail.com>
2024-03-08 19:41:49 +0100

    feat: register endpoint and routes asociated created

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,55 @@
+import bcrypt from "bcrypt"
+import User from "../models/User.js"
+import { handleError } from "../utils/handleError.js"
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
     try {
+        const firstName = req.body.firstName
+        const lastName = req.body.lastName
+        const email = req.body.email
+        const password = req.body.password
+
+        if (!firstName || !email || !password) {
+            throw new Error("all fields are mandatory")
+        }
+
+        if (password.length < 8 || password.length > 20) {
+            throw new Error("Password must contain between 8 and 20 characters")
+        }
+    
+        const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        if (!validEmail.test(email)) {
+            throw new Error("Format email invalid")
+        }
+    
+        const encryptedPassword = bcrypt.hashSync(password, 5)
+
+        const newUser = await User.create(
+            {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: encryptedPassword
+            }
+        )
        res.status(201).json(
       {
         success: true,
-        message: "Register user successfully"
+        message: "User registered successfully",
+        data: newUser
       }
     ) 
     } catch (error) {
-        handleError(res, "cant register users", 500)
+        if (error.message === "all fields are mandatory") {
+            handleError(res, error.message, 404)
+        }
+        if (error.message === "Password must contain between 8 and 20 characters") {
+            handleError(res, error.message, 400)
+        }
+        if (error.message === "Format email invalid") {
+            handleError(res, error.message, 400)
+        }
+        handleError(res, "Cant register user", 500)
     }
     
 }
--- src/models/User.js
+++ src/models/User.js
@@ -2,31 +2,34 @@ import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema(
     {
-    firstName: {
-        type: String,
-        required: true
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: false
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin", "super_admin"],
+            default: "user"
+        }
     },
-    lastName: {
-        type: String,
-        required: false
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ["user", "admin", "super_admin"],
-        default: "user"
+    {
+        timestamps: true,
+        versionKey: false
     }
-}
-{
-    timestamps: true,
-    versionKey: false
-}
 )
+
+const User = model ("User", UserSchema)
+export default User
