3a1c4a99f05273e8c8b7738eb3abb0df1f0dd725
antonio <antoniorc7@gmail.com>
2024-03-08 18:51:03 +0100

    feat:database connected in local port

---
+++ src/database/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const dbconnection = () => {
+    return mongoose.connect(
+        process.env.MONGO_URI,
+        {}
+    )
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import "dotenv/config";
+import { dbconnection } from "./database/db.js";
 
 const app = express();
 
@@ -7,6 +8,16 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 4001
 
+dbconnection()
+.then(() => {
+    console.log("Database connected")
+
+
 app.listen(PORT, () => {
     console.log(`server is running in port ${PORT}`);
 })
+})
+.catch(error => {
+    console.log(error)
+}
+)
