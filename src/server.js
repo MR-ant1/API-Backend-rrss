bb51e94427a9144df96e493f4c55383bd3afe420
antonio <antoniorc7@gmail.com>
2024-03-08 18:34:51 +0100

    feat:server running

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 4001
+
+app.listen(PORT, () => {
+    console.log(`server is running in port ${PORT}`);
+})
