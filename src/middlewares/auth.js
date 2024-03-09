53e6b58c80d5526834b1293f88693c541515127a
antonio <antoniorc7@gmail.com>
2024-03-09 15:16:58 +0100

    auth middleware creted and working properly

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+import  jwt  from "jsonwebtoken"
+import { handleError } from "../utils/handleError.js"
+
+export const auth = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+
+        if (!token) {
+            throw new Error("Couldn't authentify user")
+        }
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        )
+
+        req.tokenData = decoded
+
+        next()
+    } catch (error) {
+        if (error.message === "Couldn't authentify user") {
+            handleError(res, error.message, 400)
+        }
+        handleError(res, "Cant authentificate user", 500)
+    }
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { deleteUserById, getUsers } from "../controllers/userControllers.js";
+import { auth } from "../middlewares/auth.js"
 
 const router = Router()
 
-router.get("/", getUsers)
+router.get("/", auth, getUsers)
 router.delete("/:_id", deleteUserById)
 
 
