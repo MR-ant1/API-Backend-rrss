859aec22daee5415ce57c0a8d019a9143fde8fa3
antonio <antoniorc7@gmail.com>
2024-03-09 18:18:34 +0100

    feat: isSuperAdmin middleware created. Endpoint getUsers created at the same time to try middleware

--- src/middlewares/isSuperAdmin.js
+++ src/middlewares/isSuperAdmin.js
@@ -4,14 +4,14 @@ import { handleError } from "../utils/handleError.js"
 export const isSuperAdmin = (req, res, next) => {
     try {
         if (req.tokenData.role !== "super_admin")
-        console.log(req.tokenData.role)
         throw new Error("Unauthorized")
 
-        next()
+        next() 
+
     } catch (error) {
         if (error.message === "Unauthorized") {
             handleError(res, error.message, 400)
         }
         handleError(res, "Unathorized", 500)
     }
-}
+} 
